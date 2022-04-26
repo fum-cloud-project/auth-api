e20fbd82e6a5517c63ffff5dde073b2cccc434a4
Tooraj Taraz <tooraj.info@gmail.com>
2022-04-26 19:40:17 +0430

    clean up

--- src/actors/database/mod.rs
+++ src/actors/database/mod.rs
@@ -7,11 +7,4 @@ pub struct DbActor(pub Database);
 
 impl Actor for DbActor {
     type Context = Context<Self>;
-    fn started(&mut self, ctx: &mut Context<Self>) {
-        println!("Actor is alive");
-    }
-
-    fn stopped(&mut self, ctx: &mut Context<Self>) {
-        println!("Actor is stopped");
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,60 +12,48 @@ mod bootstrap_utils;
 mod db_schemas;
 mod middlewares;
 //local modules
+use crate::actors::database::DbActor;
 
 //external modules
-use crate::actix::Actor;
+use actix::Actor;
+use actix::Addr;
 use actix::SyncArbiter;
 use actix_web::middleware::Logger;
 use actix_web::{web::Data, App, HttpServer};
 use env_logger::Env;
 use mongodb::{options::ClientOptions, Client};
 
+//structs
+pub struct AppState {
+    pub db: Addr<DbActor>,
+    pub salt: String,
+    pub secret: String,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let db_url = dotenv!("DATABASE_URL");
-    let mut client_options = match ClientOptions::parse(db_url).await {
+    let client_options = match ClientOptions::parse(db_url).await {
         Ok(co) => co,
         _ => {
             panic!("could not parse db uri");
         }
     };
     let client = Client::with_options(client_options).unwrap();
-    if let Ok(dbs) = client.list_database_names(None, None).await {
-        for db_name in dbs {
-            println!("{}", db_name);
-        }
-    }
     let db = client.database("cloudFUMAuthDB");
-    if let Ok(cs) = db.list_collection_names(None).await {
-        println!("Created database");
-        for cs_name in cs {
-            println!("{}", cs_name);
-        }
-    }
     let actor_db = actors::database::DbActor(db);
     let db_addr = actor_db.start();
-    match db_addr
-        .clone()
-        .send(actors::database::users::CreateUser {
-            first_name: String::from("Tooraj"),
-            last_name: String::from("Taraz"),
-            email: String::from("tooraj.info@gmail.com"),
-            password: String::from("12345678"),
-            access_level: 100,
-        })
-        .await
-    {
-        Ok(res) => {
-            println!("ok {:?}", res);
-        }
-        Err(e) => {
-            println!(" err {:?}", e);
-        }
-    }
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    HttpServer::new(move || App::new().wrap(Logger::default()))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .app_data(Data::new(AppState {
+                db: db_addr.clone(),
+                salt: dotenv!("SALT_STR").to_string(),
+                secret: dotenv!("SECRET").to_string(),
+            }))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
