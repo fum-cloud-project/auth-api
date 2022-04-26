5ff9e4b43044a0e12a514b130105fb0c1271ddfd
Tooraj Taraz <tooraj.info@gmail.com>
2022-04-26 11:20:49 +0430

    base for database actor

--- src/actors/mod.rs
+++ src/actors/mod.rs
@@ -1,1 +1,10 @@
+use crate::actix::{Actor, Context};
+use mongodb::Database;
+pub mod users;
+pub mod resources;
 
+pub struct DbActor(pub Database);
+
+impl Actor for DbActor {
+    type Context = Context<Self>;
+}
