20b7252bd88415f6602c5bbec0952fc23bbb3849
Tooraj Taraz <tooraj.info@gmail.com>
2022-04-27 10:54:41 +0430

    implementing json validator and password hash and validate

--- src/utils/passwords.rs
+++ src/utils/passwords.rs
@@ -1,1 +1,19 @@
+use pwhash::bcrypt::{hash_with, verify, BcryptSetup};
 
+#[inline(always)]
+pub fn verify_password(hashed_pass: String, input_pass: String) -> bool {
+    verify(input_pass, hashed_pass.as_str())
+}
+
+pub fn hash_password(salt: String, input_pass: String) -> Result<String, ()> {
+    match hash_with(
+        BcryptSetup {
+            salt: Some(salt.as_str()),
+            ..Default::default()
+        },
+        input_pass,
+    ) {
+        Ok(res) => Ok(res),
+        _ => Err(()),
+    }
+}
--- src/utils/validations.rs
+++ src/utils/validations.rs
@@ -1,1 +1,19 @@
+use jsonschema::JSONSchema;
+use serde_json::Value;
 
+pub fn validate(schema: Value, from_api: Value) -> Result<(), Vec<String>> {
+    let compiled = JSONSchema::compile(&schema).expect("A valid schema");
+    let result = compiled.validate(&from_api);
+    let mut res: Vec<String> = vec![];
+    match result {
+        Ok(_) => {
+            return Ok(());
+        }
+        Err(errors) => {
+            for error in errors {
+                res.push(format!("{}", error));
+            }
+            return Err(res);
+        }
+    }
+}
