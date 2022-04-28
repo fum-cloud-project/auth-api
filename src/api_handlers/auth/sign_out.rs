f25befd7543cf5041e9ea7fc639b511832692410
Tooraj Taraz <tooraj.info@gmail.com>
2022-04-28 16:58:27 +0430

    fix sign out bugs

--- src/api_handlers/auth/sign_out.rs
+++ src/api_handlers/auth/sign_out.rs
@@ -14,8 +14,6 @@ use serde_json::json;
 #[post("/sign_out")]
 pub async fn sign_out(token: Json<UserDataSignOut>, state: Data<AppState>) -> impl Responder {
     let cache = state.as_ref().cache.clone();
-    let secret = &state.as_ref().secret;
-    let secret = secret.to_string();
     let token = token.into_inner();
     let token_json = match serde_json::to_value(&token) {
         Ok(val) => val,
@@ -23,15 +21,19 @@ pub async fn sign_out(token: Json<UserDataSignOut>, state: Data<AppState>) -> im
             return HttpResponse::InternalServerError().json("Something went wrong");
         }
     };
+
     match validate(get_sign_out_schema(), token_json) {
         Ok(_) => {}
         Err(e) => {
             return HttpResponse::BadRequest().json(json!({ "issues": e }));
         }
     }
-    match revoke_token(token.token.unwrap(), secret, cache.clone()).await {
-        Ok(_) => HttpResponse::Ok().json(json!({
+    match revoke_token(token.refresh_token.unwrap(), cache.clone()).await {
+        Ok(s) if s => HttpResponse::Ok().json(json!({
             "Message" : "Sing out was successful."
+        })),
+        Ok(s) if !s => HttpResponse::Unauthorized().json(json!({
+            "Message" : "Invalid token."
         })),
         _ => HttpResponse::InternalServerError().json(json!({
             "issues" : ["something went wrong"]
