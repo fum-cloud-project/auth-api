93e8f6b6db6344be1668ad9c19cc4085ada0ca83
Tooraj Taraz <tooraj.info@gmail.com>
2022-05-01 21:20:43 +0430

    implementing user get one by admin

--- src/api_handlers/users/get_one.rs
+++ src/api_handlers/users/get_one.rs
@@ -1,1 +1,30 @@
+use crate::actors::database::users::GetUserWithId;
+use crate::state::AppState;
+use actix_web::{
+    get,
+    web::{Data, Path},
+    HttpResponse, Responder,
+};
+use bson::oid::ObjectId;
+use serde_json::json;
 
+#[get("/{id}")]
+pub async fn get_one(id: Path<(String,)>, state: Data<AppState>) -> impl Responder {
+    let id = match ObjectId::parse_str(id.into_inner().0) {
+        Ok(v) => v,
+        _ => {
+            return HttpResponse::BadRequest().json(json!({ "issues": ["Bad user ID"] }));
+        }
+    };
+    let db = state.as_ref().db.clone();
+    match db.send(GetUserWithId { _id: id }).await {
+        Ok(Ok(Ok(s))) => HttpResponse::Ok().json(json!({
+            "message": "User retrieved successfully",
+            "data" : [s],
+        })),
+        Ok(Err(e)) => HttpResponse::BadRequest().json(json!({ "message": e })),
+        _ => HttpResponse::InternalServerError().json(json!({
+            "issues" : ["something went wrong"]
+        })),
+    }
+}
