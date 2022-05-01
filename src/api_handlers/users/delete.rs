8bdb86ccad8133c95f15a58c7d9dcb66d216442e
Tooraj Taraz <tooraj.info@gmail.com>
2022-05-01 21:20:08 +0430

    implementing user deletion by admin

--- src/api_handlers/users/delete.rs
+++ src/api_handlers/users/delete.rs
@@ -1,1 +1,29 @@
+use crate::actors::database::users::DeleteUser;
+use crate::state::AppState;
+use actix_web::{
+    delete,
+    web::{Data, Path},
+    HttpResponse, Responder,
+};
+use bson::oid::ObjectId;
+use serde_json::json;
 
+#[delete("/{id}")]
+pub async fn delete_admin(id: Path<(String,)>, state: Data<AppState>) -> impl Responder {
+    let id = match ObjectId::parse_str(id.into_inner().0) {
+        Ok(v) => v,
+        _ => {
+            return HttpResponse::BadRequest().json(json!({ "issues": ["Bad user ID"] }));
+        }
+    };
+    let db = state.as_ref().db.clone();
+    match db.send(DeleteUser { _id: id }).await {
+        Ok(Ok(Ok(_))) => HttpResponse::Ok().json(json!({
+            "message": "User was deleted successfully"
+        })),
+        Ok(Err(e)) => HttpResponse::BadRequest().json(json!({ "message": e })),
+        _ => HttpResponse::InternalServerError().json(json!({
+            "issues" : ["something went wrong"]
+        })),
+    }
+}
