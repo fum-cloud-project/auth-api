e48da618160c645854a52f7cadac37a3be717105
Tooraj Taraz <tooraj.info@gmail.com>
2022-05-01 20:43:36 +0430

    implementing msg for counters and pagination

--- src/actors/database/users/mod.rs
+++ src/actors/database/users/mod.rs
@@ -72,6 +72,19 @@ pub struct GetUsersWithFilter {
     pub first_name: Option<String>,
     pub last_name: Option<String>,
     pub access_level: Option<(i32, CompareType)>,
+    pub skip: u64,
+    pub limit: i64,
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<u64, u8>")]
+pub struct CountUsersWithFilter {
+    pub email: Option<String>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub access_level: Option<(i32, CompareType)>,
+    pub skip: u64,
+    pub limit: i64,
 }
 
 impl Handler<CreateUser> for DbActor {
@@ -241,6 +254,9 @@ impl Handler<GetUsersWithFilter> for DbActor {
         Box::pin(async move {
             let mut res: Vec<Users> = Vec::new();
             let mut doc_find = doc! {"is_deleted" : false};
+            let mut options = mongodb::options::FindOptions::default();
+            options.limit = Some(msg.limit);
+            options.skip = Some(msg.skip);
             if let Some(first_name) = msg.first_name {
                 doc_find.insert(
                     "first_name",
@@ -278,7 +294,7 @@ impl Handler<GetUsersWithFilter> for DbActor {
                     },
                 );
             };
-            let mut cur = match collection.find(doc_find, None).await {
+            let mut cur = match collection.find(doc_find, Some(options)).await {
                 Ok(c) => c,
                 _ => {
                     return Err(1);
@@ -295,6 +311,62 @@ impl Handler<GetUsersWithFilter> for DbActor {
                     _ => {
                         return Err(1);
                     }
+                }
+            }
+        })
+    }
+}
+
+impl Handler<CountUsersWithFilter> for DbActor {
+    type Result = ResponseFuture<Result<u64, u8>>;
+
+    fn handle(&mut self, msg: CountUsersWithFilter, _: &mut Self::Context) -> Self::Result {
+        let collection = self.0.collection::<Users>("Users");
+        Box::pin(async move {
+            let mut doc_find = doc! {"is_deleted" : false};
+            if let Some(first_name) = msg.first_name {
+                doc_find.insert(
+                    "first_name",
+                    doc! {"$regex" : format!(".*{}.*", first_name).as_str()},
+                );
+            };
+            if let Some(last_name) = msg.last_name {
+                doc_find.insert(
+                    "last_name",
+                    doc! {"$regex" : format!(".*{}.*", last_name).as_str()},
+                );
+            };
+            if let Some(email) = msg.email {
+                doc_find.insert("email", doc! {"$regex" : format!(".*{}.*", email).as_str()});
+            };
+            if let Some((access_level, cmp)) = msg.access_level {
+                doc_find.insert(
+                    "access_level",
+                    match cmp {
+                        CompareType::EQ => {
+                            doc! {"$eq" : access_level}
+                        }
+                        CompareType::GT => {
+                            doc! {"$gt" : access_level}
+                        }
+                        CompareType::LT => {
+                            doc! {"$lt" : access_level}
+                        }
+                        CompareType::GTE => {
+                            doc! {"$gte" : access_level}
+                        }
+                        CompareType::LTE => {
+                            doc! {"$lte" : access_level}
+                        }
+                    },
+                );
+            };
+            match collection.count_documents(doc_find, None).await {
+                Ok(c) => {
+                    return Ok(c);
+                }
+                _ => {
+                    return Err(1);
                 }
             }
         })
