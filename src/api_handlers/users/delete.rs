ebcf7f218ebd847052c9809b4700b412a4605e28
Tooraj Taraz <tooraj.info@gmail.com>
2022-05-02 11:37:23 +0430

    add ban on delete

--- src/api_handlers/users/delete.rs
+++ src/api_handlers/users/delete.rs
@@ -1,3 +1,4 @@
+use crate::actors::cache::tokens::BanUser;
 use crate::actors::database::users::DeleteUser;
 use crate::state::AppState;
 use actix_web::{
@@ -10,13 +11,30 @@ use serde_json::json;
 
 #[delete("/{id}")]
 pub async fn delete_admin(id: Path<(String,)>, state: Data<AppState>) -> impl Responder {
-    let id = match ObjectId::parse_str(id.into_inner().0) {
+    let id_str = id.into_inner().0;
+    let id = match ObjectId::parse_str(id_str.clone()) {
         Ok(v) => v,
         _ => {
             return HttpResponse::BadRequest().json(json!({ "issues": ["Bad user ID"] }));
         }
     };
     let db = state.as_ref().db.clone();
+    let cache = state.as_ref().cache.clone();
+    match cache
+        .send(BanUser {
+            id: id_str,
+            dur: 24 * 60 * 60,
+        })
+        .await
+    {
+        Ok(Ok(_)) => {}
+        _ => {
+            return HttpResponse::InternalServerError().json(json!({
+                "issues" : ["something went wrong"]
+            }));
+        }
+    }
+
     match db.send(DeleteUser { _id: id }).await {
         Ok(Ok(Ok(_))) => HttpResponse::Ok().json(json!({
             "message": "User was deleted successfully"
--- src/api_handlers/users/delete_acc.rs
+++ src/api_handlers/users/delete_acc.rs
@@ -1,3 +1,4 @@
+use crate::actors::cache::tokens::BanUser;
 use crate::actors::database::users::DeleteUser;
 use crate::state::AppState;
 use actix_web::{delete, web::Data, HttpMessage, HttpRequest, HttpResponse, Responder};
@@ -6,8 +7,24 @@ use serde_json::json;
 
 #[delete("/")]
 pub async fn delete(req: HttpRequest, state: Data<AppState>) -> impl Responder {
-    let id = ObjectId::parse_str(req.extensions().get::<String>().unwrap()).unwrap();
+    let id_str = req.extensions().get::<String>().unwrap().to_owned();
+    let id = ObjectId::parse_str(id_str.to_owned()).unwrap();
     let db = state.as_ref().db.clone();
+    let cache = state.as_ref().cache.clone();
+    match cache
+        .send(BanUser {
+            id: id_str,
+            dur: 24 * 60 * 60,
+        })
+        .await
+    {
+        Ok(Ok(_)) => {}
+        _ => {
+            return HttpResponse::InternalServerError().json(json!({
+                "issues" : ["something went wrong"]
+            }));
+        }
+    }
     match db.send(DeleteUser { _id: id }).await {
         Ok(Ok(Ok(_))) => HttpResponse::Ok().json(json!({
             "message": "User was deleted successfully"
